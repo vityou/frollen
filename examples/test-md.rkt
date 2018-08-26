abb93e9bfdd062e7b9101ff537c8d6672b6a9ad0
Vityou <zlee314@gmail.com>
2018-08-26 12:29:07 -0600

    move files to examples folder

---
+++ examples/test-md.rkt
@@ -0,0 +1,20 @@
+#lang frollen/markdown
+
+**asdf**
+
+@"asdf2"
+
+@(define-values (x) (values 4))
+
+@(define-meta x y)
+
+@set![x 5]
+
+@|x|asdf
+
+@y[6]
+
+x Is @x
+
+@(string-append "test " "string")
+<asdf>123</asdf>
